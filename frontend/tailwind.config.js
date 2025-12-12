ef63a6b5065799c1f561139fa56c10dbcfbf9293
AMAN KUMAR <amankumarbusiness7870@gmail.com>
2025-12-13 00:46:25 +0530

    for deployment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,13 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [require("daisyui")],
-};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
